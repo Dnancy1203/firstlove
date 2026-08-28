5c3cff27fc44a98707992f84d1adc6817d955e0a
Dnancy1203 <2635969183@qq.com>
2026-08-28 17:01:34 +0800

    更新静音链接

--- js/features.js
+++ js/features.js
@@ -186,7 +186,7 @@
 
 (function() {
     var KEY = 'keepaliveAudioEnabled';
-    var SRC = 'https://img.heliar.top/file/1772885159972_silence.m4a';
+    var SRC = 'https://raw.githubusercontent.com/Dnancy1203/firstlove/main/silence_9h.m4a';
     var _audio = null;
     var _unlockBound = false;
 
